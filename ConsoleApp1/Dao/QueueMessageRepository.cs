dea058d932174a92c95ed77552d9aae86d5a9965
shilvainhsieh_gachia <shilvainhsieh@gachia.com.tw>
2022-07-15 14:47:50 +0800

    新增說明

--- ConsoleApp1/Dao/QueueMessageRepository.cs
+++ ConsoleApp1/Dao/QueueMessageRepository.cs
@@ -10,12 +10,21 @@ namespace ConsoleApp1.Dao
 {
     public class QueueMessageRepository : AbstractEntityRepository<QueueMessage>
     {
+        /*
+        每新增一個新的 Repository，以下3部分缺一不可，
+        如有取得特定的資料，請自行新增public方法讓Program使用
+         */
+
+        //1.
         private ProjDbContext db;
+
+        //2.
         public QueueMessageRepository(ProjDbContext _db) : base(_db)
         {
             db = _db;
         }
 
+        //3.
         public override List<QueueMessage> GetEntitiesQ()
         {
             return db.QueueMessage.ToListIQ();
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,12 +11,27 @@ namespace ConsoleApp1
 {
     internal class Program
     {
-        private static QueueMessageRepository queueMessageRepository;
+        /*
+        大意：每新增一個資料表的DAO，要做以下事情：
+        1.新增 Table Class，欄位不能少且注意資料型態，該可null就要null，例如：public Datetime? dateStart { get;set; }
+        2.將新增的Table Class設定在DbContext中(請參考QueueMessage)
+        3.新增一個Repository，照QueueMessageRepository的設定方式，Repository使用詳細說明請至 QueueMessageRepository瀏覽
+
+        Program 程式如何使用QueueMessageRepository，請參考以下有標記數字的部分
+        
+         */
+
         private static ProjDbContext db;
 
+        //1.
+        private static QueueMessageRepository queueMessageRepository;
+        
+       
         public static void Init()
         {
             db = new ProjDbContext();
+
+            //2.
             queueMessageRepository = new QueueMessageRepository(db);
         }
 
@@ -24,15 +39,20 @@ namespace ConsoleApp1
         {
             Init();
 
+            Console.WriteLine("Hello World!");
+        }
+
+        public void SaveData()
+        {
+            //新增資料
+            QueueMessage queueMessage = new QueueMessage();
+            queueMessage.MessageText = DateTime.Now.ToString();
+            queueMessageRepository.Save(queueMessage, EntityState.Added);
+
+            //編輯資料
             List<QueueMessage> queueMessages = queueMessageRepository.GetEntitiesQ();
             QueueMessage queueMessage1 = queueMessages.Where(p => p.Sn == 1).FirstOrDefault();
-            queueMessage1.MessageText = DateTime.Now.ToString(); 
-            queueMessageRepository.Save(queueMessage1, EntityState.Modified);
-
-            QueueMessage queueMessage2 = queueMessages.Where(p => p.Sn == 1).FirstOrDefault();
-            queueMessageRepository.Delete(queueMessage2);
-
-            Console.WriteLine("Hello World!");
+            queueMessageRepository.Save(queueMessage, EntityState.Modified);
         }
     }
 }
